3b508c61a447c33bf3f069857cabe31d7a78118e
jonhumphreys <93622145+jonhumphreys@users.noreply.github.com>
2025-01-27 21:33:40 -0800

    Added feature dog

--- TestProject/TestProject/UnitTest1.cs
+++ TestProject/TestProject/UnitTest1.cs
@@ -9,8 +9,9 @@ public class UnitTest1
     }
     
     [Fact]
-    public void Test2()
+    public void Test2AndStringIsEmpty()
     {
         Assert.False(!true);
+        Assert.Empty(String.Empty);
     }
 }
